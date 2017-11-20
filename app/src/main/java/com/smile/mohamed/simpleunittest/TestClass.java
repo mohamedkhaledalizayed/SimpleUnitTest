9f2f3f6e1decfe469e22af2e931324dd23be1c47
mohamedkhaledalizayed <mohammedkhaled077@gmail.com>
2017-11-20 16:07:52 +0200

    Initial commit

---
+++ app/src/main/java/com/smile/mohamed/simpleunittest/TestClass.java
@@ -0,0 +1,18 @@
+package com.smile.mohamed.simpleunittest;
+
+/**
+ * Created by COMPUMAGIC on 20/11/2017.
+ */
+//https://www.youtube.com/watch?v=dlWjn4QrVK4
+public class TestClass {
+    //This Class Contain All Methods That I Want To Test It.
+
+    public int Add(int num1, int num2){
+        return num1+num2;
+    }
+
+    public int Sub(int num1, int num2){
+        return (num1-num2);
+    }
+
+}
---
+++ app/src/test/java/com/smile/mohamed/simpleunittest/TestClassTest.java
@@ -0,0 +1,31 @@
+package com.smile.mohamed.simpleunittest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by COMPUMAGIC on 20/11/2017.
+ */
+public class TestClassTest {
+    @Test
+    public void add() throws Exception {
+        int input1=10;
+        int input2=5;
+        int excepected=15;
+        TestClass aClass=new TestClass();
+        int output=aClass.Add(input1,input2);
+        assertEquals(excepected,output);
+    }
+
+    @Test
+    public void sub() throws Exception {
+        int input1=10;
+        int input2=5;
+        int excepected=5;
+        TestClass aClass=new TestClass();
+        int output=aClass.Sub(input1,input2);
+        assertEquals(excepected,output);
+    }
+
+}
